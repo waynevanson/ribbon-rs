755ea5d5140fa6e203bfd8ad97c79f493b4d57ac
Wayne Van Son <waynevanson@gmail.com>
2023-01-13 20:46:12 +1100

    tests(IndexDimensional): add `increment_step` and `increment_step_mut`

--- framework/src/index_dimensional.rs
+++ framework/src/index_dimensional.rs
@@ -7,7 +7,7 @@ pub struct IndexDimensional {
 
 impl IndexDimensional {
     pub fn increment_step_mut(&mut self) -> Option<&Self> {
-        self.inner.last_mut().map(|last| *last += 1);
+        self.inner.last_mut().map(|last| *last += 1)?;
         Some(self)
     }
 
@@ -44,5 +44,68 @@ impl<'a> IntoIterator for &'a IndexDimensional {
 
     fn into_iter(self) -> Iter<'a, usize> {
         self.inner.iter()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    mod increment_step {
+        use super::*;
+
+        #[test]
+        fn should_return_some_and_increment_existing_last_value() {
+            let result = IndexDimensional {
+                inner: vec![1, 2, 3, 4, 5, 6],
+            }
+            .increment_step();
+
+            let expected = Some(IndexDimensional {
+                inner: vec![1, 2, 3, 4, 5, 7],
+            });
+
+            assert_eq!(result, expected);
+        }
+
+        #[test]
+        fn should_return_none_when_empty() {
+            let result = IndexDimensional { inner: vec![] }.increment_step();
+
+            let expected = None;
+
+            assert_eq!(result, expected);
+        }
+    }
+
+    mod increment_step_mut {
+        use super::*;
+
+        #[test]
+        fn should_return_some_and_increment_existing_last_value() {
+            let mut data = IndexDimensional {
+                inner: vec![1, 2, 3, 4, 5, 6],
+            };
+
+            let result = data.increment_step_mut();
+
+            let next = IndexDimensional {
+                inner: vec![1, 2, 3, 4, 5, 7],
+            };
+
+            let expected = Some(&next);
+
+            assert_eq!(result, expected);
+        }
+
+        #[test]
+        fn should_return_none_when_empty() {
+            let mut data = IndexDimensional { inner: vec![] };
+            let result = data.increment_step_mut();
+
+            let expected = None;
+
+            assert_eq!(result, expected);
+        }
     }
 }
