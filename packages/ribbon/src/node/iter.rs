890bfa2df820c7c37f3c9fa837440b7501df7e5f
Wayne Van Son <waynevanson@gmail.com>
2023-01-14 01:41:21 +1100

    refactor: add node.mod

--- packages/ribbon/src/node/iter.rs
+++ packages/ribbon/src/node/iter.rs
@@ -1,60 +1,5 @@
-use std::{collections::HashMap, iter::once, ops::Deref};
-
+use super::VNode;
 use crate::index_dimensional::IndexDimensional;
-
-#[derive(Debug, PartialEq)]
-pub enum VNode {
-    Text {
-        value: String,
-    },
-    Element {
-        tag: String,
-        attributes: HashMap<String, String>,
-        children: Vec<VNode>,
-    },
-}
-
-impl VNode {
-    pub fn text(str: &str) -> Self {
-        Self::Text {
-            value: str.to_string(),
-        }
-    }
-
-    pub fn element<C, T, A, S>(tag: &str, attributes: A, children: C) -> Self
-    where
-        C: IntoIterator<Item = T>,
-        T: Into<Self>,
-        A: IntoIterator<Item = S>,
-        S: Into<(String, String)>,
-    {
-        Self::Element {
-            tag: tag.to_string(),
-            attributes: attributes.into_iter().map(|x| x.into()).collect(),
-            children: children.into_iter().map(|x| x.into()).collect(),
-        }
-    }
-
-    pub fn nth<'a>(&'a self, index: &IndexDimensional) -> Option<&'a Self> {
-        index
-            .iter()
-            .try_fold(self, |vnode, indice| vnode.child(*indice))
-    }
-
-    pub fn iter_with_index<'a>(&'a self) -> DepthIterWithIndex<'a> {
-        DepthIterWithIndex {
-            node: self,
-            index: Default::default(),
-        }
-    }
-
-    pub fn child(&self, indice: usize) -> Option<&Self> {
-        match self {
-            VNode::Text { value: _ } => None,
-            VNode::Element { children, .. } => children.iter().nth(indice),
-        }
-    }
-}
 
 pub struct DepthIterWithIndex<'a> {
     node: &'a VNode,
@@ -100,6 +45,18 @@ impl<'a> Iterator for DepthIterWithIndex<'a> {
             .unwrap_or_else(|| vec![0].into());
 
         Some(current)
+    }
+}
+
+impl<'a> IntoIterator for &'a VNode {
+    type Item = (IndexDimensional, &'a VNode);
+    type IntoIter = DepthIterWithIndex<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        DepthIterWithIndex {
+            node: self,
+            index: Default::default(),
+        }
     }
 }
 
---
+++ packages/ribbon/src/node/mod.rs
@@ -0,0 +1,56 @@
+mod iter;
+use crate::index_dimensional::IndexDimensional;
+use std::collections::HashMap;
+
+use self::iter::DepthIterWithIndex;
+
+#[derive(Debug, PartialEq)]
+pub enum VNode {
+    Text {
+        value: String,
+    },
+    Element {
+        tag: String,
+        attributes: HashMap<String, String>,
+        children: Vec<VNode>,
+    },
+}
+
+impl VNode {
+    pub fn text(str: &str) -> Self {
+        Self::Text {
+            value: str.to_string(),
+        }
+    }
+
+    pub fn element<C, T, A, S>(tag: &str, attributes: A, children: C) -> Self
+    where
+        C: IntoIterator<Item = T>,
+        T: Into<Self>,
+        A: IntoIterator<Item = S>,
+        S: Into<(String, String)>,
+    {
+        Self::Element {
+            tag: tag.to_string(),
+            attributes: attributes.into_iter().map(|x| x.into()).collect(),
+            children: children.into_iter().map(|x| x.into()).collect(),
+        }
+    }
+
+    pub fn nth<'a>(&'a self, index: &IndexDimensional) -> Option<&'a Self> {
+        index
+            .iter()
+            .try_fold(self, |vnode, indice| vnode.child(*indice))
+    }
+
+    pub fn iter_with_index<'a>(&'a self) -> DepthIterWithIndex<'a> {
+        self.into_iter()
+    }
+
+    pub fn child(&self, indice: usize) -> Option<&Self> {
+        match self {
+            VNode::Text { value: _ } => None,
+            VNode::Element { children, .. } => children.iter().nth(indice),
+        }
+    }
+}
