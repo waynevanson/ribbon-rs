36b258408934e70736ff4b5cf39aa91d1eadcf55
Wayne Van Son <waynevanson@gmail.com>
2023-01-19 22:43:14 +1100

    fix: add graph iterator

--- packages/ribbon/src/vnode/mod.rs
+++ packages/ribbon/src/vnode/mod.rs
@@ -1,8 +1,7 @@
-mod iter;
 use graph::prelude::*;
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum VNode {
     Text {
         value: String,
@@ -14,57 +13,83 @@ pub enum VNode {
     },
 }
 
+impl VNode {
+    fn for_graph(&self) -> (Vec<(usize, usize)>, Vec<&Self>) {
+        if self.children().len() == 0 {
+            (vec![], vec![self])
+        } else {
+            let mut visited_vnode = self;
+            let mut visited_node = 0;
+
+            let mut parents = vec![];
+
+            let mut edges = vec![];
+            let mut nodes = vec![self];
+
+            // currently assumes it will have more than one child,
+            // makes aprent always
+            while !parents.is_empty() || visited_node == 0 {
+                let child_node = visited_node + 1;
+
+                let child_vnode = if let Some(child_vnode) = visited_vnode.children().first() {
+                    nodes.push(child_vnode);
+                    edges.push((visited_node, child_node));
+
+                    // could check if parent has kids before adding it to the list of stuff
+                    let parent_vnode = visited_vnode;
+                    let parent_node = visited_node;
+                    let parent_breadth = 1;
+
+                    if let true = parent_vnode.child(parent_breadth + 1).is_some() {
+                        parents.push((parent_vnode, parent_node, parent_breadth));
+                    };
+
+                    child_vnode
+                } else {
+                    let &(parent_vnode, parent_node, parent_breadth) = parents
+                        .last()
+                        .expect("Deepest parent always exists in a loop where it is not empty");
+
+                    let child_vnode = parent_vnode
+                        .child(parent_breadth)
+                        .expect("Child should always exist when it comes from a parent");
+
+                    nodes.push(child_vnode);
+                    edges.push((parent_node, child_node));
+
+                    if parent_vnode.child(parent_breadth + 1).is_some() {
+                        parents
+                            .last_mut()
+                            .expect("Should exist as the parent should exist here")
+                            .2 += 1;
+                    } else {
+                        parents
+                            .pop()
+                            .expect("Should be poppable as the parent should exist here");
+                    }
+
+                    child_vnode
+                };
+
+                visited_vnode = child_vnode;
+                visited_node = child_node;
+            }
+
+            (edges, nodes)
+        }
+    }
+}
+
 // plan is to use these to perform diffs and
 impl<'a> From<&'a VNode> for DirectedCsrGraph<usize, &'a VNode, ()> {
     fn from(vnode: &'a VNode) -> Self {
-        let mut current_node = 0;
-        let mut visited: Option<(&'a VNode, usize)> = None;
-
-        let mut parents = if vnode.children().is_empty() {
-            vec![(vnode, 0, current_node)]
-        } else {
-            vec![]
-        };
-
-        let mut edges = vec![];
-        let mut nodes = vec![];
-
-        while !parents.is_empty() {
-            if let Some((current, node)) = visited {
-                let child = current.child(0);
-
-                if let Some(child) = child {
-                    // exists, add edge and node
-                    edges.push((node, current_node));
-                    nodes.push(child);
-                    parents.push((current, 0, node));
-                } else {
-                }
-            } else {
-                visited = Some((vnode, 0));
-            }
-
-            current_node += 1;
-        }
+        let (edges, nodes) = vnode.for_graph();
 
         GraphBuilder::new()
             .csr_layout(CsrLayout::Sorted)
             .edges(edges)
             .node_values(nodes)
             .build()
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn should_return_one_element_in_childless_graph() {
-        let vnode = VNode::text("zero");
-        let graph = DirectedCsrGraph::from(&vnode);
-        assert_eq!(graph.node_count(), 1);
-        assert_eq!(graph.edge_count(), 0);
     }
 }
 
@@ -101,5 +126,41 @@ impl VNode {
             VNode::Text { value: _ } => None,
             VNode::Element { children, .. } => children.iter().nth(indice),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+
+    fn should_return_two_elements_in_graph_containing_child_() {
+        let one = VNode::text("one");
+        let vnode = VNode::Element {
+            tag: "zero".to_string(),
+            attributes: Default::default(),
+            children: vec![one.clone()],
+        };
+        let result = vnode.for_graph();
+        assert_eq!(result, (vec![(0, 1)], vec![&vnode, &one]));
+    }
+
+    #[test]
+    fn should_return_one_element_in_childless_graph() {
+        let vnode = VNode::text("zero");
+        let result = vnode.for_graph();
+        assert_eq!(result, (vec![], vec![&vnode]))
+    }
+
+    #[test]
+    fn should_return_two_elements_in_graph_containing_child() {
+        let vnode = VNode::Element {
+            tag: "".to_string(),
+            attributes: Default::default(),
+            children: vec![VNode::text("zero")],
+        };
+        let graph = DirectedCsrGraph::from(&vnode);
+        assert_eq!(graph.node_count(), 2);
+        assert_eq!(graph.edge_count(), 1);
     }
 }
